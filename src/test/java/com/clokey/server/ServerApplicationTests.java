776e60c3b55ace095720ddb75af512670847f072
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-04 15:26:11 +0900

    [feat/#1] 프로젝트 초기 세팅

---
+++ src/test/java/com/clokey/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clokey.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
