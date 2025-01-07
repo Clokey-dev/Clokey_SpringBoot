137620892a8dfa8f2cabd2d99f00ef4e0bb56d67
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-01-07 16:21:26 +0900

    [feat/#12] ClothFolder 엔티티 생성

---
+++ src/main/java/com/clokey/server/domain/model/mapping/ClothFolder.java
@@ -0,0 +1,27 @@
+package com.clokey.server.domain.model.mapping;
+
+import com.clokey.server.domain.model.BaseEntity;
+import com.clokey.server.domain.model.Category;
+import com.clokey.server.domain.model.Cloth;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ClothFolder extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cloth_id")
+    private Cloth cloth;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
