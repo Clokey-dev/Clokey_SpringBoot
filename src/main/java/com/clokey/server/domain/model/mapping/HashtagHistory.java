0ccbb34205e99cdeae18df0d6ffd04fb62c3f9ee
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-01-07 16:46:58 +0900

    [feat/#12] Hashtag 관련 엔티티 생성
    Hashtag 와 HashtagHistory 구현 완료

---
+++ src/main/java/com/clokey/server/domain/model/mapping/HashtagHistory.java
@@ -0,0 +1,28 @@
+package com.clokey.server.domain.model.mapping;
+
+import com.clokey.server.domain.model.BaseEntity;
+import com.clokey.server.domain.model.Cloth;
+import com.clokey.server.domain.model.Hashtag;
+import com.clokey.server.domain.model.History;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class HashtagHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hashtag_id")
+    private Hashtag hashtag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "history_id")
+    private History history;
+}
