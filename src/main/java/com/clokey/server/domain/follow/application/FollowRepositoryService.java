bdc075503b7f2dd496f2ef2516b5b0b5b14625f4
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-01-10 01:34:34 +0900

    [fix/#14] 맞팔확인 메서드 삭제

--- src/main/java/com/clokey/server/domain/follow/application/FollowRepositoryService.java
+++
@@ -1,8 +0,0 @@
-package com.clokey.server.domain.follow.application;
-
-public interface FollowRepositoryService {
-
-    //서로 맞팔인지 확인합니다.
-    boolean isFriends(Long memberId1, Long memberId2);
-
-}
