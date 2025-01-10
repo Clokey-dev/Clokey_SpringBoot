425c932c66fc8eacaa66289c487ac510465a0242
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-01-10 17:32:14 +0900

    [feat/#14] HistoryResponseDto 구현 완료

--- src/main/java/com/clokey/server/domain/history/dto/HistoryResponseDto.java
+++ src/main/java/com/clokey/server/domain/history/dto/HistoryResponseDto.java
@@ -1,16 +1,11 @@
 package com.clokey.server.domain.history.dto;
 
-import com.clokey.server.domain.model.BaseEntity;
-import com.clokey.server.domain.model.Member;
-import com.clokey.server.domain.model.enums.Visibility;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class HistoryResponseDto {
@@ -20,17 +15,16 @@ public class HistoryResponseDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class dayViewResult{
-        Long memberId;
+        Long userId;
         String contents;
         List<String> imageUrl;
         List<String> hashtags;
-        String visibility;
-        Integer likeCount;
-
+        boolean visibility;
+        int likeCount;
+        boolean isLiked;
+        LocalDate date;
     }
 }
-
-
 
 
 
