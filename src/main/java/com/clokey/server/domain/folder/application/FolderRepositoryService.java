b32b6f7d2903544672045b5c95cda5a4916ac894
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-11 05:02:08 +0900

    [feat/#19] FolderRepositoryService save 함수 추가

--- src/main/java/com/clokey/server/domain/folder/application/FolderRepositoryService.java
+++ src/main/java/com/clokey/server/domain/folder/application/FolderRepositoryService.java
@@ -1,6 +1,9 @@
 package com.clokey.server.domain.folder.application;
 
+import com.clokey.server.domain.model.Folder;
+
 public interface FolderRepositoryService {
 
     boolean folderExist(Long folderId);
+    void saveFolder(Folder folder);
 }
