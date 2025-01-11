dd2308f541ac4d4e69294f782006b53c5548c458
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-01-11 19:47:12 +0900

    [feat/#14] 월별 조회 기능 구현 완료

--- src/main/java/com/clokey/server/domain/history/api/HistoryRestController.java
+++ src/main/java/com/clokey/server/domain/history/api/HistoryRestController.java
@@ -6,6 +6,7 @@ import com.clokey.server.domain.MemberLike.application.MemberLikeRepositoryServi
 import com.clokey.server.domain.history.converter.HistoryConverter;
 import com.clokey.server.domain.history.application.HistoryRepositoryService;
 import com.clokey.server.domain.history.dto.HistoryResponseDto;
+import com.clokey.server.domain.member.application.MemberRepositoryService;
 import com.clokey.server.domain.model.History;
 import com.clokey.server.domain.model.enums.Visibility;
 import com.clokey.server.global.common.response.BaseResponse;
@@ -34,6 +35,7 @@ public class HistoryRestController {
     private final HistoryRepositoryService historyRepositoryService;
     private final HashtagHistoryRepositoryService hashtagHistoryRepositoryService;
     private final MemberLikeRepositoryService memberLikeRepositoryService;
+    private final MemberRepositoryService memberRepositoryService;
 
     //임시로 엔드 포인트 맨 뒤에 멤버 Id를 받도록 했습니다 토큰에서 나의 id를 가져올 수 있도록 수정해야함.
     //이유는 isLiked를 확인해야 하기 때문입니다. ㅠ ㅠ
@@ -44,7 +46,7 @@ public class HistoryRestController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "HISTORY_4002", description = "존재하지 않는 기록ID 입니다.",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
     })
     @Parameters({
-            @Parameter(name = "history_id", description = "기록의 id, path variable 입니다.")
+            @Parameter(name = "historyId", description = "기록의 id, path variable 입니다.")
     })
     public BaseResponse<HistoryResponseDto.dayViewResult> getDaily(@PathVariable Long historyId, @PathVariable Long memberId) {
 
@@ -73,7 +75,7 @@ public class HistoryRestController {
         return BaseResponse.onSucesss(SuccessStatus.HISTORY_SUCCESS,HistoryConverter.toDayViewResult(history.get(),imageUrl,hashtags,likeCount,isLiked));
     }
 
-    // 권한이 없는 경우를 추가해야함.
+    // 권한이 없는 경우를 추가해야함. -> 월의 볼 수 있는 경우와 그렇지 않은 경우가 섞여있다면? 하... ㅠ
     //임시로 멤버 Id를 받도록 했습니다 토큰에서 나의 id를 가져올 수 있도록 수정해야함.
     @GetMapping("/monthly/{this_member_id}/")
     @Operation(summary = "특정 멤버의 특정 월의 기록을 확인할 수 있는 API",description = "query parameter로 member_id와 month를 입력해주세요(YYYY-MM) 형태.")
@@ -82,14 +84,22 @@ public class HistoryRestController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "HISTORY_4002", description = "존재하지 않는 기록ID 입니다.",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
     })
     @Parameters({
-            @Parameter(name = "history_id", description = "기록의 id, path variable 입니다.")
+            @Parameter(name = "memberId", description = "조회하고자 하는 memberId, 빈칸 입력시 현재 유저를 기준으로 합니다."),
+            @Parameter(name = "month", description = "조회하고자 하는 월입니다. YYYY-MM 형식으로 입력해주세요.")
     })
     public BaseResponse<HistoryResponseDto.monthViewResult> getMonthlyHistories(@PathVariable Long this_member_id,
-            @RequestParam(value = "member_id", required = false) Long memberId,
+            @RequestParam(value = "memberId", required = false) Long memberId,
             @RequestParam(value = "month") String month) {
 
         //memberId가 null 이라면 this_member_id 즉 지금 이용자의 Id를 기준으로 찾습니다.
         Long searchMemberId = (memberId != null) ? memberId : this_member_id;
+
+        //임시 validation
+        // 존재 하지 않는 멤버인 경우.
+        if(!memberRepositoryService.memberExist(searchMemberId)) {
+            return BaseResponse.onFailure(ErrorStatus.NO_SUCH_MEMBER,null);
+        }
+
 
         List<History> histories = historyRepositoryService.getMemberHistoryByYearMonth(searchMemberId, month);
         List<String> historyImageUrls = historyRepositoryService.getFirstImageUrlsOfHistory(histories);
