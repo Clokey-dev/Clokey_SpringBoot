a495fed506d8004eaae15bafbfa1d1fa07ff39ed
2ghrms <aa020228@gmail.com>
2025-01-16 19:08:52 +0900

    [feat/#46] Cloth 서비스구현체 생성

--- src/main/java/com/clokey/server/domain/cloth/application/ClothServiceImpl.java
+++ src/main/java/com/clokey/server/domain/cloth/application/ClothServiceImpl.java
@@ -1,4 +1,12 @@
 package com.clokey.server.domain.cloth.application;
 
-public class ClothServiceImpl {
+import com.clokey.server.domain.cloth.dao.ClothRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClothServiceImpl implements ClothService {
+
+    private final ClothRepository clothRepository;
 }
