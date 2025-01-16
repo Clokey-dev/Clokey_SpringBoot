5965fc950bc7ff29017b239b0d581761604e304e
2ghrms <aa020228@gmail.com>
2025-01-16 19:07:18 +0900

    [feat/#46] Cloth 컨트롤러 생성

--- src/main/java/com/clokey/server/domain/cloth/api/ClothRestController.java
+++ src/main/java/com/clokey/server/domain/cloth/api/ClothRestController.java
@@ -1,4 +1,13 @@
 package com.clokey.server.domain.cloth.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/clothes")
+@Validated
 public class ClothRestController {
 }
