6ac8296e95d7280b78184d04b553fc8141786ad5
2ghrms <aa020228@gmail.com>
2025-01-16 19:06:56 +0900

    [feat/#46] Cloth 응답/요청 DTO 생성

--- src/main/java/com/clokey/server/domain/cloth/dto/ClothRequestDto.java
+++ src/main/java/com/clokey/server/domain/cloth/dto/ClothRequestDto.java
@@ -1,4 +1,49 @@
 package com.clokey.server.domain.cloth.dto;
 
+import com.clokey.server.domain.model.enums.Season;
+import com.clokey.server.domain.model.enums.ThicknessLevel;
+import com.clokey.server.domain.model.enums.Visibility;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class ClothRequestDto {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClothCreateRequest{
+        String name;
+        Season season;
+        int tempUpperBound;
+        int tempLowerBound;
+        ThicknessLevel thicknessLevel;
+        Visibility visibility;
+        String clothUrl;
+        String brand;
+        Long memberId;
+        Long categoryId;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClothUpdateRequest{
+        String name;
+        Season season;
+        int tempUpperBound;
+        int tempLowerBound;
+        ThicknessLevel thicknessLevel;
+        Visibility visibility;
+        String clothUrl;
+        String brand;
+        Long memberId;
+        Long categoryId;
+    }
 }
--- src/main/java/com/clokey/server/domain/cloth/dto/ClothResponseDto.java
+++ src/main/java/com/clokey/server/domain/cloth/dto/ClothResponseDto.java
@@ -1,4 +1,75 @@
 package com.clokey.server.domain.cloth.dto;
 
+import com.clokey.server.domain.model.enums.Season;
+import com.clokey.server.domain.model.enums.ThicknessLevel;
+import com.clokey.server.domain.model.enums.Visibility;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class ClothResponseDto {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClothCreateResult{
+        Long id;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClothReadResult{
+        Long id;
+        String name;
+        int wearNum;
+        LocalDate regDate;
+        Season season;
+        int tempUpperBound;
+        int tempLowerBound;
+        ThicknessLevel thicknessLevel;
+        Visibility visibility;
+        String clothUrl;
+        String brand;
+        List<String> images;
+        Long memberId;
+        Long categoryId;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClothUpdateResult{
+        Long id;
+        List<String> updateFields;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClothPreview {
+        private Long id;
+        private String name;
+        private String mainImage;
+        private int wearNum;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CategoryClothResult {
+        private List<ClothPreview> clothes;
+        private int listSize;
+        private int totalPage;
+        private long totalElements;
+        private boolean isFirst;
+        private boolean isLast;
+    }
+
 }
