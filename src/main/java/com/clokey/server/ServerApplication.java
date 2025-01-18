d82b7c82d52259ad2040361b0cb0a55331065b5e
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-18 16:34:05 +0900

    [refactor/#56] ServerApplication.java 수정

--- src/main/java/com/clokey/server/ServerApplication.java
+++ src/main/java/com/clokey/server/ServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@PropertySource("classpath:properties/env.properties") // env.properties 파일 소스 등록 -> 환경변수 설정
 public class ServerApplication {
 
 	public static void main(String[] args) {
