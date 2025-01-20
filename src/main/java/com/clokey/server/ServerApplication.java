c7423d4b491303faa9c6c57e8b80c16b6b13ec80
2ghrms <aa020228@gmail.com>
2025-01-20 16:37:35 +0900

    [feat/#46] Application에서 properties 관련 설정 삭제

--- src/main/java/com/clokey/server/ServerApplication.java
+++ src/main/java/com/clokey/server/ServerApplication.java
@@ -2,12 +2,10 @@ package com.clokey.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@PropertySource("classpath:properties/env.properties") // env.properties 파일 소스 등록 -> 환경변수 설정
 public class ServerApplication {
 
 	public static void main(String[] args) {
