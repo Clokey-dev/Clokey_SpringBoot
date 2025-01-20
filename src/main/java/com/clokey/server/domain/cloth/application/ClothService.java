843152d8527d1b33314cf4a90e32ae753acdf997
2ghrms <aa020228@gmail.com>
2025-01-20 16:38:32 +0900

    [feat/#46] ClothService 메소드 생성

--- src/main/java/com/clokey/server/domain/cloth/application/ClothService.java
+++ src/main/java/com/clokey/server/domain/cloth/application/ClothService.java
@@ -1,4 +1,14 @@
 package com.clokey.server.domain.cloth.application;
 
+import com.clokey.server.domain.model.Cloth;
+
+import java.util.Optional;
+
 public interface ClothService {
+
+    boolean clothExist(Long clothId);
+
+    boolean isPublic(Long clothId);
+
+    Optional<Cloth> getClothById(Long clothId);
 }
--- src/main/java/com/clokey/server/domain/cloth/application/ClothServiceImpl.java
+++ src/main/java/com/clokey/server/domain/cloth/application/ClothServiceImpl.java
@@ -1,12 +1,40 @@
 package com.clokey.server.domain.cloth.application;
 
+import com.clokey.server.domain.cloth.converter.ClothConverter;
 import com.clokey.server.domain.cloth.dao.ClothRepository;
+import com.clokey.server.domain.cloth.dto.ClothResponseDto;
+import com.clokey.server.domain.model.Cloth;
+import com.clokey.server.domain.model.enums.ThicknessLevel;
+import com.clokey.server.domain.model.enums.Visibility;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ClothServiceImpl implements ClothService {
 
     private final ClothRepository clothRepository;
+
+    @Override
+    public boolean clothExist(Long clothId) {
+        System.out.println(clothId);
+        return clothRepository.existsById(clothId);
+    }
+
+    @Override
+    public boolean isPublic(Long clothId) {
+        Visibility visibility = clothRepository.findById(clothId)
+                .get()
+                .getVisibility();
+        return visibility.equals(Visibility.PUBLIC);
+    }
+
+    public Optional<Cloth> getClothById(Long clothId) {
+        return clothRepository.findById(clothId);
+    }
+
 }
