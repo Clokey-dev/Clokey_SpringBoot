44b79973d3c2b35397bdce2792b5ebe5ca213622
2ghrms <aa020228@gmail.com>
2025-01-21 17:08:44 +0900

    [refactor/#69] regDate, dayOfWeek, seasons 조회 응답 추가

--- src/main/java/com/clokey/server/domain/cloth/converter/ClothConverter.java
+++ src/main/java/com/clokey/server/domain/cloth/converter/ClothConverter.java
@@ -3,18 +3,33 @@ package com.clokey.server.domain.cloth.converter;
 import com.clokey.server.domain.cloth.dto.ClothResponseDto;
 import com.clokey.server.domain.model.entity.Cloth;
 import com.clokey.server.domain.model.entity.ClothImage;
+import com.clokey.server.domain.model.entity.enums.Season;
 
+import java.time.format.TextStyle;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class ClothConverter {
 
     public static ClothResponseDto.ClothReadResult toClothReadResult(Cloth cloth) {
+
+        // CreatedAt으로 등록일자 가져오기 / LocalDateTime -> Date 변환
+        Date regDate = java.sql.Date.valueOf(cloth.getCreatedAt().toLocalDate());
+
+        // 요일 가져오기
+        String dayOfWeek = cloth.getCreatedAt().getDayOfWeek()
+                .getDisplayName(TextStyle.SHORT, Locale.ENGLISH)
+                .toUpperCase(); // 대문자로 출력
+
         return ClothResponseDto.ClothReadResult.builder()
                 .id(cloth.getId())
                 .name(cloth.getName())
                 .wearNum(cloth.getWearNum())
-                .regDate(cloth.getRegDate())
-                .season(cloth.getSeason())
+                .regDate(regDate)
+                .dayOfWeek(dayOfWeek)
+                .seasons(cloth.getSeasons())
                 .tempUpperBound(cloth.getTempUpperBound())
                 .tempLowerBound(cloth.getTempLowerBound())
                 .thicknessLevel(cloth.getThicknessLevel())
