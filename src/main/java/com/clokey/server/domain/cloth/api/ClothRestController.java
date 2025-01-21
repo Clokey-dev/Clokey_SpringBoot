02e19f6be29052525e82ba183c3bd963af71190e
2ghrms <aa020228@gmail.com>
2025-01-21 17:11:42 +0900

    [refactor/#69] DTO 이름 변경된 것 적용

--- src/main/java/com/clokey/server/domain/cloth/api/ClothRestController.java
+++ src/main/java/com/clokey/server/domain/cloth/api/ClothRestController.java
@@ -1,12 +1,9 @@
 package com.clokey.server.domain.cloth.api;
 
 import com.clokey.server.domain.cloth.application.ClothService;
-import com.clokey.server.domain.cloth.converter.ClothConverter;
-import com.clokey.server.domain.model.repository.ClothRepository;
-import com.clokey.server.domain.cloth.dto.ClothResponseDto;
+import com.clokey.server.domain.cloth.dto.ClothResponseDTO;
 import com.clokey.server.domain.cloth.exception.annotation.ClothExist;
 import com.clokey.server.domain.cloth.exception.validator.ClothAccessibleValidator;
-import com.clokey.server.domain.model.entity.Cloth;
 import com.clokey.server.global.common.response.BaseResponse;
 import com.clokey.server.global.error.code.status.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,8 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/clothes")
@@ -28,7 +23,6 @@ public class ClothRestController {
 
     private final ClothService clothService;
     private final ClothAccessibleValidator clothAccessibleValidator;
-    private final ClothRepository clothRepository;
 
     // 옷 상세 조회 API, 사용자 토큰 받는 부분 추가 및 변경해야함
     @GetMapping("/{clothId}")
@@ -39,14 +33,15 @@ public class ClothRestController {
     @Parameters({
             @Parameter(name = "clothId", description = "옷의 id, path variable 입니다.")
     })
-    public BaseResponse<ClothResponseDto.ClothReadResult> getClothDetails(@PathVariable @Valid @ClothExist Long clothId, @RequestParam Long memberId) {
+    public BaseResponse<ClothResponseDTO.ClothReadResult> getClothDetails(@PathVariable @Valid @ClothExist Long clothId, @RequestParam Long memberId) {
 
         // 멤버가 옷에 대해서 접근 권한이 있는지 확인합니다. -> 토큰을 이용해서 현재 로그인 중인 memberId 뽑아와서 넣어줄 것. 조회하는 현 유저를 나타냄
         clothAccessibleValidator.validateClothAccessOfMember(clothId, memberId);
 
-        Optional<Cloth> cloth = clothService.readClothById(clothId);
+        // ClothService를 통해 데이터를 가져오고, 결과 반환
+        ClothResponseDTO.ClothReadResult result = clothService.readClothDetailsById(clothId, memberId);
 
-        return BaseResponse.onSuccess(SuccessStatus.CLOTH_SUCCESS, ClothConverter.toClothReadResult(cloth.get()));
+        return BaseResponse.onSuccess(SuccessStatus.CLOTH_SUCCESS, result);
     }
 
     // 옷 삭제 API, 사용자 토큰 받는 부분 추가 및 변경해야함
@@ -58,7 +53,7 @@ public class ClothRestController {
     @Parameters({
             @Parameter(name = "clothId", description = "옷의 id, path variable 입니다.")
     })
-    public BaseResponse<ClothResponseDto.ClothReadResult> deleteCloth(@PathVariable @Valid @ClothExist Long clothId, @RequestParam Long memberId) {
+    public BaseResponse<ClothResponseDTO.ClothReadResult> deleteCloth(@PathVariable @Valid @ClothExist Long clothId, @RequestParam Long memberId) {
 
         // 멤버가 옷에 대해서 수정 권한이 있는지 확인합니다. -> 토큰을 이용해서 현재 로그인 중인 memberId 뽑아와서 넣어줄 것. 삭제하는 현 유저를 나타냄
         clothAccessibleValidator.validateClothEditOfMember(clothId, memberId);
