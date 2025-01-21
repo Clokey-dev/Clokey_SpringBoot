6935e6b95c129d96d3e59ce5703381b442804e52
2ghrms <aa020228@gmail.com>
2025-01-21 17:11:23 +0900

    [refactor/#69] 응답 형식 변경된 것 convertor에 적용

--- src/main/java/com/clokey/server/domain/cloth/converter/ClothConverter.java
+++ src/main/java/com/clokey/server/domain/cloth/converter/ClothConverter.java
@@ -1,19 +1,16 @@
 package com.clokey.server.domain.cloth.converter;
 
-import com.clokey.server.domain.cloth.dto.ClothResponseDto;
+import com.clokey.server.domain.cloth.dto.ClothResponseDTO;
 import com.clokey.server.domain.model.entity.Cloth;
 import com.clokey.server.domain.model.entity.ClothImage;
-import com.clokey.server.domain.model.entity.enums.Season;
 
 import java.time.format.TextStyle;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 public class ClothConverter {
 
-    public static ClothResponseDto.ClothReadResult toClothReadResult(Cloth cloth) {
+    public static ClothResponseDTO.ClothReadResult toClothReadResult(Cloth cloth) {
 
         // CreatedAt으로 등록일자 가져오기 / LocalDateTime -> Date 변환
         Date regDate = java.sql.Date.valueOf(cloth.getCreatedAt().toLocalDate());
@@ -23,7 +20,7 @@ public class ClothConverter {
                 .getDisplayName(TextStyle.SHORT, Locale.ENGLISH)
                 .toUpperCase(); // 대문자로 출력
 
-        return ClothResponseDto.ClothReadResult.builder()
+        return ClothResponseDTO.ClothReadResult.builder()
                 .id(cloth.getId())
                 .name(cloth.getName())
                 .wearNum(cloth.getWearNum())
