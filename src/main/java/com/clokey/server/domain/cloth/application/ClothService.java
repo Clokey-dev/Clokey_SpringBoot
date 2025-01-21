7d941e9b5fb5aaf11ad1fe2bf61732519374dfa0
2ghrms <aa020228@gmail.com>
2025-01-21 17:10:41 +0900

    [refactor/#69] 서비스단 리팩토링

--- src/main/java/com/clokey/server/domain/cloth/application/ClothService.java
+++ src/main/java/com/clokey/server/domain/cloth/application/ClothService.java
@@ -1,22 +1,12 @@
 package com.clokey.server.domain.cloth.application;
 
-import com.clokey.server.domain.cloth.dto.ClothRequestDto;
-import com.clokey.server.domain.cloth.dto.ClothResponseDto;
-import com.clokey.server.domain.model.entity.Cloth;
-import jakarta.validation.Valid;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Optional;
+import com.clokey.server.domain.cloth.dto.ClothResponseDTO;
 
 public interface ClothService {
 
-    boolean clothExist(Long clothId);
-
-    boolean isPublic(Long clothId);
-
     //public ClothResponseDto.ClothCreateResult createCloth(ClothRequestDto.ClothCreateRequest request, MultipartFile imageFile);
 
-    Optional<Cloth> readClothById(Long clothId);
+    ClothResponseDTO.ClothReadResult readClothDetailsById(Long clothId, Long memberId);
 
     void deleteClothById(Long clothId);
 }
