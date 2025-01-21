5107570e20f21d0520029ee6d2baf0a16f437b65
2ghrms <aa020228@gmail.com>
2025-01-21 11:49:43 +0900

    [feat/#66] Cloth 연관 레포지토리에 delete 메소드 추가

--- src/main/java/com/clokey/server/domain/model/repository/ClothRepository.java
+++ src/main/java/com/clokey/server/domain/model/repository/ClothRepository.java
@@ -1,16 +1,21 @@
 package com.clokey.server.domain.model.repository;
 
 import com.clokey.server.domain.model.entity.Cloth;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
 public interface ClothRepository extends JpaRepository<Cloth, Long> {
 
+    boolean existsById(Long id);
+
     @EntityGraph(attributePaths = {"images"})
     Optional<Cloth> findById(Long id);
 
-    @Override
-    boolean existsById(Long aLong);
+    @Transactional
+    @Modifying
+    void deleteById(Long id);
 }
