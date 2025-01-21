9c343857895d7017500cea6d3753c231120f00d4
2ghrms <aa020228@gmail.com>
2025-01-21 11:50:01 +0900

    [feat/#66] Cloth 서비스단에 delete메소드 추가

--- src/main/java/com/clokey/server/domain/cloth/application/ClothService.java
+++ src/main/java/com/clokey/server/domain/cloth/application/ClothService.java
@@ -1,6 +1,10 @@
 package com.clokey.server.domain.cloth.application;
 
+import com.clokey.server.domain.cloth.dto.ClothRequestDto;
+import com.clokey.server.domain.cloth.dto.ClothResponseDto;
 import com.clokey.server.domain.model.entity.Cloth;
+import jakarta.validation.Valid;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
@@ -10,5 +14,9 @@ public interface ClothService {
 
     boolean isPublic(Long clothId);
 
-    Optional<Cloth> getClothById(Long clothId);
+    //public ClothResponseDto.ClothCreateResult createCloth(ClothRequestDto.ClothCreateRequest request, MultipartFile imageFile);
+
+    Optional<Cloth> readClothById(Long clothId);
+
+    void deleteClothById(Long clothId);
 }
--- src/main/java/com/clokey/server/domain/cloth/application/ClothServiceImpl.java
+++ src/main/java/com/clokey/server/domain/cloth/application/ClothServiceImpl.java
@@ -1,8 +1,12 @@
 package com.clokey.server.domain.cloth.application;
 
+import com.clokey.server.domain.model.repository.ClothFolderRepository;
+import com.clokey.server.domain.model.repository.ClothImageRepository;
 import com.clokey.server.domain.model.repository.ClothRepository;
 import com.clokey.server.domain.model.entity.Cloth;
 import com.clokey.server.domain.model.entity.enums.Visibility;
+import com.clokey.server.domain.model.repository.HistoryClothRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,10 @@ import java.util.Optional;
 public class ClothServiceImpl implements ClothService {
 
     private final ClothRepository clothRepository;
+    private final ClothImageRepository clothImageRepository;
+    private final ClothFolderRepository clothFolderRepository;
+    private final HistoryClothRepository historyClothRepository;
+    //private final S3Uploader s3Uploader; // S3 업로드를 위한 의존성
 
     @Override
     public boolean clothExist(Long clothId) {
@@ -28,8 +36,31 @@ public class ClothServiceImpl implements ClothService {
         return visibility.equals(Visibility.PUBLIC);
     }
 
-    public Optional<Cloth> getClothById(Long clothId) {
+//    public ClothResponseDto.ClothCreateResult createCloth(ClothRequestDto.ClothCreateRequest request, MultipartFile imageFile) {
+//
+//        // Cloth 엔티티 생성
+//        Cloth cloth = clothRepository.save(ClothConverter.toClothCreateRequest(request));
+//
+//        // 이미지 업로드 후 URL 저장
+//        String imageUrl = s3Uploader.upload(imageFile, "cloth-images");
+//        ClothImage clothImage = ClothImage.builder()
+//                .imageUrl(imageUrl)
+//                .cloth(cloth)
+//                .build();
+//        clothImageRepository.save(clothImage);
+//
+//        return ClothConverter.toResponse(cloth);
+//    }
+
+    public Optional<Cloth> readClothById(Long clothId) {
         return clothRepository.findById(clothId);
     }
 
+    @Transactional
+    public void deleteClothById(Long clothId){
+        historyClothRepository.deleteAllByClothId(clothId);
+        clothFolderRepository.deleteAllByClothId(clothId);
+        clothImageRepository.deleteAllByClothId(clothId);
+        clothRepository.deleteById(clothId);
+    }
 }
