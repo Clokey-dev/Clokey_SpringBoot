1711e72332c06c2c159215926f7ab91af96617f7
2ghrms <aa020228@gmail.com>
2025-01-24 01:19:30 +0900

    [feat/#63] 옷 생성 응답과 옷 생성 convert 메소드 작성

--- src/main/java/com/clokey/server/domain/cloth/converter/ClothConverter.java
+++ src/main/java/com/clokey/server/domain/cloth/converter/ClothConverter.java
@@ -1,14 +1,23 @@
 package com.clokey.server.domain.cloth.converter;
 
+import com.clokey.server.domain.cloth.dto.ClothRequestDTO;
 import com.clokey.server.domain.cloth.dto.ClothResponseDTO;
+import com.clokey.server.domain.model.entity.Category;
 import com.clokey.server.domain.model.entity.Cloth;
 import com.clokey.server.domain.model.entity.ClothImage;
+import com.clokey.server.domain.model.entity.Member;
 
 import java.time.format.TextStyle;
 import java.util.Date;
 import java.util.Locale;
 
 public class ClothConverter {
+
+    public static ClothResponseDTO.ClothCreateResult toClothCreateResult(Cloth cloth) {
+        return ClothResponseDTO.ClothCreateResult.builder()
+                .id(cloth.getId())
+                .build();
+    }
 
     public static ClothResponseDTO.ClothReadResult toClothReadResult(Cloth cloth) {
 
@@ -38,6 +47,33 @@ public class ClothConverter {
                         .toList()) // ClothImage의 imageUrl만 추출
                 .memberId(cloth.getMember().getId())
                 .categoryId(cloth.getCategory().getId())
+                .build();
+    }
+
+    /*
+    public static ClothResponseDTO.ClothUpdateResult toClothUpdateResult(Cloth cloth) {
+        return ClothResponseDTO.ClothUpdateResult.builder()
+                .id(cloth.getId())
+                .updateFields()
+                .build();
+    }*/
+
+    public static Cloth toCloth(ClothRequestDTO.ClothCreateRequest request) {
+        return Cloth.builder()
+                .name(request.getName())
+                .seasons(request.getSeasons()) // List<Season> 직접 할당
+                .tempUpperBound(request.getTempUpperBound())
+                .tempLowerBound(request.getTempLowerBound())
+                .thicknessLevel(request.getThicknessLevel())
+                .visibility(request.getVisibility())
+                .clothUrl(request.getClothUrl())
+                .brand(request.getBrand())
+                .member(Member.builder()
+                        .id(request.getMemberId())
+                        .build()) // Member를 간단히 생성 (참조만 설정)
+                .category(Category.builder()
+                        .id(request.getCategoryId())
+                        .build()) // Category를 간단히 생성 (참조만 설정)
                 .build();
     }
 }
