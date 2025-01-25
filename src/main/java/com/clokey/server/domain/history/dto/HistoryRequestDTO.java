2f6371e9504ca81a79b5ff7a41ddca4513b3dc3d
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-01-25 17:11:29 +0900

    [feat/#82] 요청과 출력 DTO 구현

--- src/main/java/com/clokey/server/domain/history/dto/HistoryRequestDTO.java
+++ src/main/java/com/clokey/server/domain/history/dto/HistoryRequestDTO.java
@@ -3,6 +3,7 @@ package com.clokey.server.domain.history.dto;
 import com.clokey.server.domain.history.exception.annotation.ContentLength;
 import com.clokey.server.domain.history.exception.annotation.HistoryExist;
 import com.clokey.server.domain.history.exception.annotation.ParentCommentConditions;
+import com.clokey.server.domain.model.entity.enums.Visibility;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -10,7 +11,27 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 public class HistoryRequestDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class HistoryCreate {
+
+        String content;
+
+        List<Long> clothes;
+
+        List<String> hashtags;
+
+        Visibility visibility;
+
+        String date;
+    }
+
 
     @Builder
     @Getter
