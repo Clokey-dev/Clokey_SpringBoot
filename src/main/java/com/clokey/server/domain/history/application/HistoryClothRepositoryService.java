48622d041b6b9a937ae72e413284669a95559b1a
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-01-26 16:06:15 +0900

    [refactor/#85] 옷 기록 횟수 count 로직 레포지토리 서비스단으로 리펙토링

--- src/main/java/com/clokey/server/domain/history/application/HistoryClothRepositoryService.java
+++ src/main/java/com/clokey/server/domain/history/application/HistoryClothRepositoryService.java
@@ -1,9 +1,11 @@
 package com.clokey.server.domain.history.application;
 
+import com.clokey.server.domain.cloth.domain.entity.Cloth;
+import com.clokey.server.domain.history.domain.entity.History;
 import com.clokey.server.domain.history.domain.entity.HistoryCloth;
 
 public interface HistoryClothRepositoryService {
 
-    void save(HistoryCloth historyCloth);
+    void save(History history, Cloth cloth);
 
 }
--- src/main/java/com/clokey/server/domain/history/application/HistoryClothRepositoryServiceImpl.java
+++ src/main/java/com/clokey/server/domain/history/application/HistoryClothRepositoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.clokey.server.domain.history.application;
 
+import com.clokey.server.domain.cloth.domain.entity.Cloth;
+import com.clokey.server.domain.history.domain.entity.History;
 import com.clokey.server.domain.history.domain.entity.HistoryCloth;
 import com.clokey.server.domain.history.domain.repository.HistoryClothRepository;
 import jakarta.transaction.Transactional;
@@ -14,7 +16,11 @@ public class HistoryClothRepositoryServiceImpl implements HistoryClothRepository
     private final HistoryClothRepository historyClothRepository;
 
     @Override
-    public void save(HistoryCloth historyCloth) {
-        historyClothRepository.save(historyCloth);
+    public void save(History history , Cloth cloth) {
+        cloth.increaseWearNum();
+        historyClothRepository.save(HistoryCloth.builder()
+                        .history(history)
+                        .cloth(cloth)
+                        .build());
     }
 }
