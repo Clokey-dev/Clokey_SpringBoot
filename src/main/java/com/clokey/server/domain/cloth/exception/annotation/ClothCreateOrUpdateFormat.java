c23a83c516b20830608be0cb61ce710e7dd5a404
2ghrms <aa020228@gmail.com>
2025-01-28 18:18:04 +0900

    [feat/#77] Cloth 생성/수정 포맷 validator 작성

---
+++ src/main/java/com/clokey/server/domain/cloth/exception/annotation/ClothCreateOrUpdateFormat.java
@@ -0,0 +1,18 @@
+package com.clokey.server.domain.cloth.exception.annotation;
+
+import com.clokey.server.domain.cloth.exception.validator.ClothCreateOrUpdateFormatValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ClothCreateOrUpdateFormatValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClothCreateOrUpdateFormat {
+
+    String message() default "생성/수정 포맷이 올바르지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
