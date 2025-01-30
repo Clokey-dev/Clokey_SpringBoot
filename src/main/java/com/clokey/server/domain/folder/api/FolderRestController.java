393b5c3cc14ed8fa98ed1e4d48503c68b55fc0f4
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-30 18:01:16 +0900

    [feat/#75] HTTPS status code 수정 및 swagger 설정 추가

--- src/main/java/com/clokey/server/domain/folder/api/FolderRestController.java
+++ src/main/java/com/clokey/server/domain/folder/api/FolderRestController.java
@@ -8,6 +8,7 @@ import com.clokey.server.domain.folder.exception.annotation.FolderExist;
 import com.clokey.server.global.common.response.BaseResponse;
 import com.clokey.server.global.error.code.status.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,9 @@ public class FolderRestController {
 
     @Operation(summary = "폴더 생성 API", description = "폴더 생성하는 API입니다.")
     @PostMapping("/folders")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FOLDER_201", description = "성공적으로 생성되었습니다."),
+    })
     public BaseResponse<FolderResponseDTO.FolderIdDTO> createFolder(@RequestParam Long memberId,
                                                                     @RequestBody FolderRequestDTO.FolderCreateRequest request) {
         FolderResponseDTO.FolderIdDTO response = FolderConverter.toFolderIdDTO(folderService.createFolder(memberId, request));
@@ -28,6 +32,9 @@ public class FolderRestController {
 
     @Operation(summary = "폴더 삭제 API", description = "폴더 삭제하는 API입니다.")
     @DeleteMapping("/folders/{folderId}")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FOLDER_204", description = "성공적으로 삭제되었습니다."),
+    })
     public BaseResponse<String> deleteFolder(@RequestParam Long memberId,
                                              @FolderExist @PathVariable Long folderId) {
         folderService.deleteFolder(folderId, memberId);
@@ -36,6 +43,9 @@ public class FolderRestController {
 
     @Operation(summary = "폴더 이름 수정 API", description = "폴더 이름 수정하는 API입니다.")
     @PatchMapping("/folders/{folderId}")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FOLDER_204", description = "성공적으로 수정되었습니다."),
+    })
     public BaseResponse<String> editFolderName(@RequestParam Long memberId,
                                                @RequestBody FolderRequestDTO.FolderEditRequest request) {
         folderService.editFolderName(request.getFolderId(), request.getNewName(), memberId);
@@ -44,6 +54,9 @@ public class FolderRestController {
 
     @Operation(summary = "폴더에 옷 추가 API", description = "폴더에 옷 추가하는 API입니다.")
     @PatchMapping("folders/{folderId}/clothes")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FOLDER_201", description = "성공적으로 추가되었습니다."),
+    })
     public BaseResponse<String> addClothesToFolder(@RequestParam Long memberId,
                                                    @RequestBody FolderRequestDTO.AddClothesToFolderRequest request) {
         folderService.addClothesToFolder(request, memberId);
--- src/main/java/com/clokey/server/global/error/code/status/SuccessStatus.java
+++ src/main/java/com/clokey/server/global/error/code/status/SuccessStatus.java
@@ -29,9 +29,9 @@ public enum SuccessStatus implements BaseCode {
     //폴더 성공
     FOLDER_SUCCESS(HttpStatus.OK, "FOLDER_200", "성공적으로 조회되었습니다."),
     FOLDER_CREATED(HttpStatus.CREATED, "FOLDER_201", "성공적으로 생성되었습니다."),
-    FOLDER_DELETED(HttpStatus.OK, "FOLDER_200", "성공적으로 삭제되었습니다."),
-    FOLDER_EDIT_SUCCESS(HttpStatus.OK, "FOLDER_200", "성공적으로 수정되었습니다."),
-    FOLDER_ADD_CLOTHES_SUCCESS(HttpStatus.OK, "FOLDER_200", "성공적으로 추가되었습니다."),
+    FOLDER_DELETED(HttpStatus.NO_CONTENT, "FOLDER_204", "성공적으로 삭제되었습니다."),
+    FOLDER_EDIT_SUCCESS(HttpStatus.NO_CONTENT, "FOLDER_204", "성공적으로 수정되었습니다."),
+    FOLDER_ADD_CLOTHES_SUCCESS(HttpStatus.CREATED, "FOLDER_201", "성공적으로 추가되었습니다."),
 
     //검색 성공
     SEARCH_SUCCESS(HttpStatus.OK, "SEARCH_200", "성공적으로 조회되었습니다."),
