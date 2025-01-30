6d9d8004e9b5c081c97be8e2e1672e5c84fdd366
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-30 22:41:52 +0900

    [refactor/#75] NotBlank, NotEmpty 추가
    - 입력값 검증 추가

--- src/main/java/com/clokey/server/domain/folder/dto/FolderRequestDTO.java
+++ src/main/java/com/clokey/server/domain/folder/dto/FolderRequestDTO.java
@@ -1,15 +1,20 @@
 package com.clokey.server.domain.folder.dto;
 
+import com.clokey.server.domain.cloth.exception.annotation.ClothExist;
 import com.clokey.server.domain.folder.exception.annotation.FolderExist;
+import jakarta.persistence.ElementCollection;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
-@NotNull
 public class FolderRequestDTO {
     @Getter
     public static class FolderCreateRequest {
+        @NotBlank(message = "폴더 이름은 필수 입력 값입니다.")
         String folderName;
     }
 
@@ -17,6 +22,7 @@ public class FolderRequestDTO {
     public static class FolderEditRequest {
         @FolderExist
         Long folderId;
+        @NotBlank(message = "newName은 필수 입력 값입니다.")
         String newName;
     }
 
@@ -24,6 +30,8 @@ public class FolderRequestDTO {
     public static class AddClothesToFolderRequest {
         @FolderExist
         Long folderId;
+
+        @NotEmpty(message = "clothesId은 필수 입력 값입니다.")
         List<Long> clothesId;
     }
 }
