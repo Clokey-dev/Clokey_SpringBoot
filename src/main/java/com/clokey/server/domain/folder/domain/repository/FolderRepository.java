f6d9ced49a8f7d605712ef235a4f8245d44da607
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-01-30 17:00:11 +0900

    [refactor/#75] conflict 해결

--- src/main/java/com/clokey/server/domain/folder/domain/repository/FolderRepository.java
+++ src/main/java/com/clokey/server/domain/folder/domain/repository/FolderRepository.java
@@ -1,7 +1,9 @@
 package com.clokey.server.domain.folder.domain.repository;
 
 import com.clokey.server.domain.folder.domain.entity.Folder;
+import com.clokey.server.domain.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    boolean existsByIdAndOwner(Long folderId, Member owner);
 }
