02ad932827e11e66545620c5c56920369705ece7
2ghrms <aa020228@gmail.com>
2025-01-31 18:17:30 +0900

    [feat/#65] 생성/수정 DTO valid 추가

--- src/main/java/com/clokey/server/domain/cloth/dto/ClothRequestDTO.java
+++ src/main/java/com/clokey/server/domain/cloth/dto/ClothRequestDTO.java
@@ -1,8 +1,12 @@
 package com.clokey.server.domain.cloth.dto;
 
+import com.clokey.server.domain.cloth.exception.annotation.ClothCreateOrUpdateFormat;
 import com.clokey.server.domain.model.entity.enums.Season;
 import com.clokey.server.domain.model.entity.enums.ThicknessLevel;
 import com.clokey.server.domain.model.entity.enums.Visibility;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,11 +21,21 @@ public class ClothRequestDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class ClothCreateOrUpdateRequest {
+        @NotNull
         private String name;
+        @NotNull
         private List<Season> seasons;
+        @NotNull
+        @Min(-20)
+        @Max(40)
         private Integer tempUpperBound;
+        @NotNull
+        @Min(-20)
+        @Max(40)
         private Integer tempLowerBound;
+        @NotNull
         private ThicknessLevel thicknessLevel;
+        @NotNull
         private Visibility visibility;
         private String clothUrl;
         private String brand;
