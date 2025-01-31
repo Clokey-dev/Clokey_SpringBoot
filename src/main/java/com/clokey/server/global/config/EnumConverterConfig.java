0a56c1569073f6c489051568051b45fb4d218443
2ghrms <aa020228@gmail.com>
2025-01-31 18:16:21 +0900

    [feat/#65] 소문자 enum upper case컨터버 추가

---
+++ src/main/java/com/clokey/server/global/config/EnumConverterConfig.java
@@ -0,0 +1,30 @@
+package com.clokey.server.global.config;
+
+import com.clokey.server.global.converter.StringToEnumConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
+
+@Configuration
+public class EnumConverterConfig implements WebMvcConfigurer {
+
+    private static final List<Class<? extends Enum<?>>> ENUM_CLASSES = List.of(
+            com.clokey.server.domain.model.entity.enums.ClothSort.class,
+            com.clokey.server.domain.model.entity.enums.Season.class,
+            com.clokey.server.domain.model.entity.enums.SummaryFrequency.class
+            );
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        for (Class<? extends Enum<?>> enumClass : ENUM_CLASSES) {
+            registry.addConverter(createConverter(enumClass));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends Enum<T>> StringToEnumConverter<T> createConverter(Class<? extends Enum<?>> enumClass) {
+        return new StringToEnumConverter<>((Class<T>) enumClass);
+    }
+}
+
