f35a798069b63509dd49beaf31903dbf5c51a327
2ghrms <aa020228@gmail.com>
2025-02-01 16:48:37 +0900

    [feat/#114] 옷 생성/조회시 DTO에 not blank 추가

--- src/main/java/com/clokey/server/domain/cloth/dto/ClothRequestDTO.java
+++ src/main/java/com/clokey/server/domain/cloth/dto/ClothRequestDTO.java
@@ -6,6 +6,7 @@ import com.clokey.server.domain.model.entity.enums.ThicknessLevel;
 import com.clokey.server.domain.model.entity.enums.Visibility;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,20 +23,26 @@ public class ClothRequestDTO {
     @AllArgsConstructor
     public static class ClothCreateOrUpdateRequest {
         @NotNull
+        @NotBlank
         private String name;
         @NotNull
+        @NotBlank
         private List<Season> seasons;
         @NotNull
+        @NotBlank
         @Min(-20)
         @Max(40)
         private Integer tempUpperBound;
         @NotNull
+        @NotBlank
         @Min(-20)
         @Max(40)
         private Integer tempLowerBound;
         @NotNull
+        @NotBlank
         private ThicknessLevel thicknessLevel;
         @NotNull
+        @NotBlank
         private Visibility visibility;
         private String clothUrl;
         private String brand;
