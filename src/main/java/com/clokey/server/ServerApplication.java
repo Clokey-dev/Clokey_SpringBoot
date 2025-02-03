9090ede07917b410cadea225d534d278d6c6dd0f
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-02-03 19:23:21 +0900

    [feat/#123] redis 설정 완료

--- src/main/java/com/clokey/server/ServerApplication.java
+++ src/main/java/com/clokey/server/ServerApplication.java
@@ -2,11 +2,12 @@ package com.clokey.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationProperties("spring.data.redis")
 public class ServerApplication {
 
 	public static void main(String[] args) {
