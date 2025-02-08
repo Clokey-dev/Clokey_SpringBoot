ae099c72273c27765fc00109df28bf6c8714e5ad
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-02-08 15:20:55 +0900

    [feat/#150] NotificationResponseDTO 구현

---
+++ src/main/java/com/clokey/server/domain/notification/dto/NotificationResponseDTO.java
@@ -0,0 +1,63 @@
+package com.clokey.server.domain.notification.dto;
+
+import com.clokey.server.domain.history.dto.HistoryResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class NotificationResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class HistoryLikeNotificationResult {
+        String content;
+        String memberProfileUrl;
+        Long historyId;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class NewFollowerNotificationResult {
+        String content;
+        String memberProfileUrl;
+        Long memberId;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class HistoryCommentNotificationResult {
+        String content;
+        String memberProfileUrl;
+        Long historyId;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReplyNotificationResult {
+        String content;
+        String memberProfileUrl;
+        Long historyId;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class NewHistoryNotificationResult {
+        String content;
+        String historyImageUrl;
+        Long historyId;
+    }
+}
