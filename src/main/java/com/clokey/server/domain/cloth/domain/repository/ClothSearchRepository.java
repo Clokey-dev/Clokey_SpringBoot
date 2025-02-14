efe4726d9a245fdecdced0de1809a6583a0faf0e
2ghrms <aa020228@gmail.com>
2025-02-14 22:34:33 +0900

    [feat/#159] Java Client API 방식의 ES 사용하기 위해 삭제

--- src/main/java/com/clokey/server/domain/cloth/domain/repository/ClothSearchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clokey.server.domain.cloth.domain.repository;
-
-import com.clokey.server.domain.cloth.domain.document.ClothDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import java.util.List;
-
-public interface ClothSearchRepository extends ElasticsearchRepository<ClothDocument, String> {
-    List<ClothDocument> findByNameAndBrand(String name, String brand);
-}
