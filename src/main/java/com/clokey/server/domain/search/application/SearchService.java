c115b94da7f4037be759accd0e89f5d682255bb2
2ghrms <aa020228@gmail.com>
2025-02-15 16:30:09 +0900

    [feat/#183] 동기화 및 검색 메소드와 API 작성

--- src/main/java/com/clokey/server/domain/search/application/SearchService.java
+++ src/main/java/com/clokey/server/domain/search/application/SearchService.java
@@ -1,6 +1,9 @@
 package com.clokey.server.domain.search.application;
 
 import com.clokey.server.domain.cloth.dto.ClothResponseDTO;
+import com.clokey.server.domain.member.domain.document.MemberDocument;
+import com.clokey.server.domain.member.dto.MemberDTO;
+import org.springframework.data.domain.Page;
 
 import java.io.IOException;
 
@@ -9,4 +12,13 @@ public interface SearchService {
     void syncClothesDataToElasticsearch() throws IOException;
 
     ClothResponseDTO.ClothPreviewListResult searchClothesByNameOrBrand(String keyword, int page, int size) throws IOException;
+
+    void syncMembersDataToElasticsearch() throws IOException;
+
+    MemberDTO.ProfilePreviewListRP searchMembersByClokeyIdOrNickname(String keyword, int page, int size) throws IOException;
+
+//    void syncHistoriesDataToElasticsearch() throws IOException;
+//
+//    MemberDTO.ProfilePreviewListRP searchMembersByClokeyIdOrNickname(String keyword, int page, int size) throws IOException;
+
 }
