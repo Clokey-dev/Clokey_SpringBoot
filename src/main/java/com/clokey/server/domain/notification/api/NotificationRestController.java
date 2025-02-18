56f7a6b1cf54f0ad993d313aef2f3cf15eec875d
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-02-18 14:36:57 +0900

    [refactor/#220] 비활성화 사용자에게 알림을 보낼 수 없습니다.

--- src/main/java/com/clokey/server/domain/notification/api/NotificationRestController.java
+++ src/main/java/com/clokey/server/domain/notification/api/NotificationRestController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/notification")
+@RequestMapping("/notifications")
 @Validated
 public class NotificationRestController {
 
