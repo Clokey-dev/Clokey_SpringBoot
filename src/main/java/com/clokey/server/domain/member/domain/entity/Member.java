31e035ba0fd307e399e5b206c1b357add2878711
paeng <127924700+juuuuone@users.noreply.github.com>
2025-02-21 04:12:45 +0900

    [fix/#274] 이메일 유니크 제약

--- src/main/java/com/clokey/server/domain/member/domain/entity/Member.java
+++ src/main/java/com/clokey/server/domain/member/domain/entity/Member.java
@@ -34,7 +34,7 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     @Column(length = 30)
